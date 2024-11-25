d6bf1e04604c1477c9b80352c2ea2ce3e07db657
rodlferreira <rodriggo.lferreira@icloud.com>
2024-11-25 22:10:13 +0000

    endpoint aws-api-gateway create method POST finished

--- src/main/java/com/java/createUrlShortner/UrlData.java
+++ src/main/java/com/java/createUrlShortner/UrlData.java
@@ -1,30 +1,13 @@
 package com.java.createUrlShortner;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UrlData {
     private String originalUrl;
     private long expirationTimeInSeconds;
-
-    public UrlData() {}
-
-    public UrlData(String originalUrl, long expirationTimeInSeconds) {
-        this.originalUrl = originalUrl;
-        this.expirationTimeInSeconds = expirationTimeInSeconds;
-    }
-
-    // Getters e Setters
-    public String getOriginalUrl() {
-        return originalUrl;
-    }
-
-    public void setOriginalUrl(String originalUrl) {
-        this.originalUrl = originalUrl;
-    }
-
-    public long getExpirationTimeInSeconds() {
-        return expirationTimeInSeconds;
-    }
-
-    public void setExpirationTimeInSeconds(long expirationTimeInSeconds) {
-        this.expirationTimeInSeconds = expirationTimeInSeconds;
-    }
 }
